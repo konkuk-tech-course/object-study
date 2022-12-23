baada8e06af781061c90a1d91a25584b7bb89c98
khyojun <nandong5923@naver.com>
2022-12-23 14:57:19 +0900

    오류 수정

---
+++ khyojun/object_ch02/src/main/java/org/example/DiscountPolicy.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public interface DiscountPolicy {
+
+
+    Money calculateDiscountAmount(Screening screening);
+
+
+}
